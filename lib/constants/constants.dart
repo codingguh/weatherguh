3175f2677f5ea21a7e94b063e827a8d82556374e
codingguh <teguhharits91@gmail.com>
2024-10-09 23:27:04 +0700

    first commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class Constants {
+  static const String apiKey = '9559f252ba5bac1dd8f683a9e027e25b';
+  static const String apiKey2 = '9559f252ba5bac1dd8f683a9e027e25b';
+}
